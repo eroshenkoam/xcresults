10b4048db8b2333c228669f3f677df5f4dd0068d
Artem Eroshenko <eroshenkoam@me.com>
2023-08-28 13:31:15 +0300

    fix null

--- src/main/java/io/eroshenkoam/xcresults/carousel/Carousel.java
+++ src/main/java/io/eroshenkoam/xcresults/carousel/Carousel.java
@@ -1,14 +1,16 @@
 package io.eroshenkoam.xcresults.carousel;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Carousel implements Serializable {
 
     private final List<CarouselImage> images;
 
     public Carousel(final List<CarouselImage> images) {
-        this.images = images;
+        this.images = Optional.ofNullable(images).orElseGet(ArrayList::new);
     }
 
     public List<CarouselImage> getImages() {
